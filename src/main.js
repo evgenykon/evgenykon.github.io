5f8a981ddac5556778b0ee3e49032ecca085a8d9
Евгений Коноплев <evgeny.konoplev@gemotest.ru>
2022-07-28 00:14:58 +0300

    ya metrika

--- src/main.js
+++ src/main.js
@@ -16,4 +16,16 @@ export default function (Vue, { router, head, isClient }) {
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  
+  if (isClient) {
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+    ym(88933609, "init", {
+         clickmap:true,
+         trackLinks:true,
+         accurateTrackBounce:true
+    });
+  }
 }
